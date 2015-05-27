7ee82e03d384db41935f947d5bfee10db9ff7436
ShelbyZ <user@shagman.codes>
2015-05-26 22:59:38 -0500

    Removing express dependency and general code cleanup

--- proxy.js
+++ proxy.js
@@ -2,146 +2,164 @@
 
 var http = require('http');
 var https = require('https');
-var express = require('express');
 var url = require('url');
 
-function createPlatform () {
-    var platform = function (options) {
-        var app = express();
+function createProxy () {
+    var proxy = function (options) {
         var server;
 
-        setupExpress(app, options.port, options.logging);
+        logging = options.logging;
 
         if (options.secure) {
-            server = https.createServer(options.cert, app).listen(options.port);
+            server = https.createServer(options.cert, handleRequest).listen(options.port);
         } else {
-            server = http.createServer(app).listen(options.port);
+            server = http.createServer(null, handleRequest).listen(options.port);
         }
-    };
+    },
+    logging;
 
-    function setupExpress (app, port, logging) {
-        var endpoints = {};
-
-        function logObject (item) {
-            if (logging) {
-                console.log(item);
+    function handleRequest (request, response) {
+        if (request.method.toLowerCase() !== 'post') {
+            response.writeHead(405);
+            response.end();
+        } else {
+            var uri = url.parse(request.url);
+            if (uri.pathname === '/proxy') {
+                proxyRequest(request, response);
+            } else if (uri.pathname.toLowerCase() === '/proxy') {
+                response.writeHead(307, {
+                    Location: '/proxy'
+                });
+                response.end();
             }
         }
+    }
 
-        function getHeader (headers, name) {
-            for (var i in headers) {
-                if (i.toLowerCase() === name.toLowerCase()) {
-                    return headers[i];
-                }
-            }
-        }
+    function proxyRequest (request, response) {
+        var buffer = '';
 
-        function setHeader (headers, name, value) {
-            for (var i in headers) {
-                if (i.toLowerCase() === name.toLowerCase()) {
-                    headers[i] = value;
-                    return;
-                }
-            }
+        request.on('data', function (data) {
+            buffer += data;
+        });
 
-            headers[name.toLowerCase()] = value;
-        }
-
-        function deleteHeader (headers, name) {
-            for (var i in headers) {
-                if (i.toLowerCase() === name.toLowerCase()) {
-                    delete headers[i];
-                    break;
-                }
-            }
-        }
-
-        function createRequest (uri, obj, callback) {
-            var request;
-
-            obj.hostname = uri.hostname;
-            obj.path = uri.path;
-            obj.port = uri.port;
-
-            if (uri.protocol === 'https:') {
-                request = https.request(obj, callback);
-            } else {
-                request = http.request(obj, callback);
-            }
-
-            return request;
-        }
-
-        app.post('/proxy', function (request, response) {
-            request.setEncoding('utf8');
-            request.on('data', function (data) {
-                var obj = JSON.parse(data);
-                var uri = url.parse(obj.url);
-                var length = 0;
-                var contentType;
-                obj.method = obj.method;
-
-                if (!obj.headers) {
-                    obj.headers = {};
-                }
-
-                obj.rejectUnauthorized = false;
-
-                if (!obj.data) {
-                    deleteHeader(obj.headers, 'content-type');
-                    deleteHeader(obj.headers, 'content-length');
-                } else {
-                    contentType = getHeader(obj.headers, 'content-type');
-                    length = obj.data.length;
-
-                    if (!contentType) {
-                        setHeader(obj.headers, 'content-type', 'application/json');
-                        contentType = getHeader(obj.headers, 'content-type');
-                    }
-
-                    if (contentType === 'application/json') {
-                        obj.data = JSON.stringify(obj.data);
-                    }
-
-                    length = obj.data.length;
-
-                    setHeader(obj.headers, 'content-length', length);
-                }
-
-                deleteHeader(obj.headers, 'accept-encoding');
-                deleteHeader(obj.headers, 'host');
-
-                logObject(JSON.stringify(obj));
-
-                var req = createRequest(uri, obj, function (res) {
-                    var body = '';
-                    res.on('data', function (d) {
-                        body += d;
-                    });
-
-                    res.on('end', function () {
-                        response.writeHead(res.statusCode, res.headers);
-                        response.write(body);
-                        response.end();
-                    });
+        request.on('end', function () {
+            var obj = parseRequest(buffer),
+            req = createRequest(obj, function (res) {
+                buffer = '';
+                res.on('data', function (data) {
+                    buffer += data;
                 });
 
-                if (obj.data) {
-                    req.write(obj.data, 'utf8');
-                }
-
-                req.on('error', function (error) {
-                    req.abort();
-                    response.writeHead(404);
+                res.on('end', function () {
+                    response.writeHead(res.statusCode, res.headers);
+                    response.write(buffer);
                     response.end();
                 });
-
-                req.end();
             });
+
+            if (obj.data) {
+                req.write(obj.data, 'utf8');
+            }
+
+            req.on('error', function (error) {
+                req.abort();
+                response.writeHead(404);
+                response.end();
+            });
+
+            req.end();
         });
     }
 
-    return platform;
+    function parseRequest (data) {
+        var obj = JSON.parse(data),
+        length = 0,
+        contentType;
+
+        obj.uri = url.parse(obj.url);
+        obj.method = obj.method;
+        obj.hostname = obj.uri.hostname;
+        obj.path = obj.uri.path;
+        obj.port = obj.uri.port;
+
+        if (!obj.headers) {
+            obj.headers = {};
+        }
+
+        obj.rejectUnauthorized = false;
+
+        if (!obj.data) {
+            processHeader(obj.headers, 'delete', 'content-type');
+            processHeader(obj.headers, 'delete', 'content-length');
+        } else {
+            contentType = processHeader(obj.headers, 'get', 'content-type');
+            length = obj.data.length;
+
+            if (!contentType) {
+                processHeader(obj.headers, 'set', 'content-type', 'application/json');
+                contentType = processHeader(obj.headers, 'get', 'content-type');
+            }
+
+            if (contentType === 'application/json') {
+                obj.data = JSON.stringify(obj.data);
+            }
+
+            length = obj.data.length;
+
+            processHeader(obj.headers, 'set', 'content-length', length);
+        }
+
+        processHeader(obj.headers, 'delete', 'accept-encoding');
+        processHeader(obj.headers, 'delete', 'host');
+
+        logObject(JSON.stringify(obj));
+
+        return obj;
+    }
+
+    function processHeader (headers, action, name, value) {
+        for (var i in headers) {
+            if (i.toLowerCase() === name.toLowerCase()) {
+                switch (action) {
+                    case 'get':
+                        return headers[i];
+                    case 'set':
+                        headers[i] = value;
+                        return;
+                    case 'delete':
+                        delete headers[i];
+                        return;
+                }
+            }
+        }
+
+        if (action === 'set') {
+            headers[name.toLowerCase()] = value;
+        }
+    }
+
+    function logObject (item) {
+        if (logging) {
+            console.log(item);
+        }
+    }
+
+    function createRequest (obj, callback) {
+        var request,
+        protocol = obj.uri.protocol;
+        
+        delete obj.uri;
+
+        if (protocol === 'https:') {
+            request = https.request(obj, callback);
+        } else {
+            request = http.request(obj, callback);
+        }
+
+        return request;
+    }
+
+    return proxy;
 }
 
-module.exports = createPlatform();
+module.exports = createProxy();
